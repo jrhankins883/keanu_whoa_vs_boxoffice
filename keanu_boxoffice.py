4a58b50e7636496f78e9975af18ec3dc1ef129a1
John Hankins <jrhankins883@yahoo.com>
2024-12-05 16:42:20 -0500

    Haven't added much new code. I created a requirements.txt file and started using an ipynb file to help me work through data easier.

--- keanu_boxoffice.py
+++ keanu_boxoffice.py
@@ -1,6 +1,8 @@
 import requests
-# I'm establishing the variables I need in order to access the Keanu Reeves Filmography on TMDb
+import tmdbsimple as tmdb
 
+# I'm establishing the variables I need in order to access the Keanu Reeves Filmography on TMDb
+API_KEY = '93b29bf0b851209878217fccd1d8ca1a'
 base_URL = "https://api.themoviedb.org/3"
 url = f"{base_URL}/person/{6384}/movie_credits?api_key={API_KEY}"
 print(f"Requesting URL: {url}")
